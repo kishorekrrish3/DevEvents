d6b91579dea4787f5e050c6139be5ff713fb13de
Kishore P <kidkrrish3@gmail.com>
2025-12-01 21:52:39 +0530

    Loaded real events

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Page = async () => {
   'use cache';
   cacheLife('hours')
-  const response = await fetch(`${BASE_URL}/api/events`);
+  const response = await fetch(`https://${BASE_URL}/api/events`);
   const { events } = await response.json();
 
   return (
